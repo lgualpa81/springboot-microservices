c2c8738a8bb572fbeafeddcf8f65183255d7d530
lgualpa <lgualpa.dev@gmail.com>
2024-08-07 17:35:31 -0500

    03. Configure spring security and keycloak

---
+++ apiGateway/src/main/java/com/apigw/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.apigw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        .authorizeExchange(auth -> {
+          auth.anyExchange()
+              .authenticated();
+        })
+        .oauth2Login(Customizer.withDefaults());
+    return http.build();
+  }
+}
--- productService/src/main/java/com/products/controllers/ProductController.java
+++ productService/src/main/java/com/products/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.products.model.dto.ProductResponse;
 import com.products.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +18,14 @@ public class ProductController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   public void addProduct(@RequestBody ProductRequest payload) {
     this.productService.addProduct(payload);
   }
 
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
+  @PreAuthorize("hasRole('ROLE_USER')")
   public List<ProductResponse> getAllProducts() {
     return this.productService.getAllProducts();
   }
